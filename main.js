4ccec9b512d1a0509b38a54917c764b62da7b0cc
Nicole Hashimoto de Azevedo <107440308+Nicolehashimotoazevedo@users.noreply.github.com>
2024-04-16 11:40:10 -0300

    rsrsrs

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function verificarTime() {
     var respostaTime = prompt("Qual o melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "CARROS) {
+    if (respostaTime.toLowerCase() === ("CARROS
+  ) {
       alert(SONATA);
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
